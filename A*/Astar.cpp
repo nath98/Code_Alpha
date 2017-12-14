ee7663779c1997984ad4a6cdae8e37e0888bfaf0
Mylintil <quentin.goudard@gmail.com>
2017-12-14 12:32:08 +0100

    Version fonctionnel

--- A*/Astar.cpp
+++ A*/Astar.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <math.h>
+#include <fstream>
 
 #define X 6 //taille matrice
 #define Y 6
@@ -11,7 +12,7 @@ void create_grid (char (*matrix)[Y], struct node (*grid)[Y]);
 void add_openlist_around_node(struct node (*grid)[Y], struct node *openlist[X*Y], struct c_lists *C, struct node N,  struct Closedlist *closedlist);
 void print_grid( struct node (*grid)[Y] );
 void grid_H_calcul ( struct node (*grid)[Y], struct node end);
-
+void find_path ( struct node (*grid)[Y], unsigned short size, node end_node, node start_node);
 struct node {
 
 	unsigned short x, y, C, H, F; //position x, y, cout à l'origine, cout Heuristique, cout total
@@ -53,11 +54,11 @@ void Astar(struct node start, struct node end) {
 	char matrix[X][Y] = {  'f', 'f' ,'f', 'f', 'f', 'f' ,'f', 'f', 'b', 'f', 'f' ,'f', 'b', 'b', 'f' ,'f','f', 'f' ,'f', 'b', 'f', 'f' ,'f', 'f', 'b', 'f', 'f' ,'f', 'b', 'b', 'f' ,'f','b','f','f','f'  };
 	struct node grid[X][Y];
 	struct node *openlist[X*Y];
-	struct Closedlist closedlist;//*parents *enfants
+	struct Closedlist closedlist;// *parents *enfants
 	struct c_lists C = { //compteur openlist et closedlist
 			 0,0
 	};
-	unsigned short openlist_stop=0, i;
+	unsigned short openlist_stop=0, i, open_empty=0;
 	struct smaller_F S_M;
 
 	create_grid(matrix, grid);
@@ -93,31 +94,45 @@ void Astar(struct node start, struct node end) {
 		//std::cout << C.open;
 
 		//openlist_stop=1;
+		open_empty=1;
+		S_M.i=0;
+		S_M.v=65535;
 		for(i=0; i<C.open; i++)
 		{
 
 			if(openlist[i]->state=='o'){
 				if((openlist[i]->x==end.x)&&(openlist[i]->y==end.y)){
 					openlist_stop=1;
-					cout << "SEMAPHORE";
+					cout << "STOP";
 
 				}
 				else{
 					if(openlist[i]->F<=S_M.v){
 						S_M.v = openlist[i]->F;
 						S_M.i = i;
-						//cout<<C.open ;
 
 					}
 				}
+				open_empty=0;
 			}
 		}
-		add_openlist_around_node(grid, openlist, &C, *openlist[S_M.i], &closedlist);
-		cout << "NON";
+		if (openlist[S_M.i]->state=='o'){
+			add_openlist_around_node(grid, openlist, &C, *openlist[S_M.i], &closedlist);
+		}
+		else{
+			openlist_stop=1;
+		}
+
+		if(open_empty){
+			openlist_stop=1;
+		}
+		cout << "Fin for" << endl;
 
 
 	}
-	cout << "fin" ;
+
+	find_path ( grid, C.open, *openlist[C.open] , start);
+	cout << "FIN" ;
 
 
 
@@ -188,9 +203,7 @@ void print_grid( struct node (*grid)[Y] ){
 	for (i=0; i<X; i++){
 		for(y=0; y<Y; y++){
 			std::cout<<grid[i][y].state<<" ";
-			/*if(grid[i][y].state == 'o'){
-				std::cout<<"("<<grid[i][y].F<<") ";
-			}*/
+
 
 		}
 		std::cout<<std::endl;
@@ -205,4 +218,47 @@ void grid_H_calcul ( struct node (*grid)[Y], struct node end){
 			grid[i][y].H = hypot(grid[i][y].x-end.x,grid[i][y].y-end.y);
 		}
 	}
+}
+
+void find_path ( struct node (*grid)[Y], unsigned short size, node end_node, node start_node){
+
+	unsigned short path[2*size];
+	signed short i, y, c_path=0;
+	node local_node, temp_node;
+	ofstream file("test.txt", ios::out | ios::trunc);
+
+	local_node = end_node;
+
+	cout<<local_node.x << endl;
+	cout<<local_node.y << endl;
+	while((local_node.x!=start_node.x)&&(local_node.y!=start_node.y)){
+		for (i=-1; i<=1; i++){
+			for (y=-1; y<=1; y++){
+				/*if( //Dont exceed the grid
+				(grid[local_node.x+i][local_node.y+y].x>=0) //Dont exceed in left
+				&& (grid[local_node.x+i][local_node.y+y].y>=0) //Dont exceed in top
+				&& (grid[local_node.x+i][local_node.y+y].x<X) //Dont exceed in right
+				&& (grid[local_node.x+i][local_node.y+y].y<Y) ) //Dont exceed in bottom
+				{*/
+					//cout << local_node.C << endl;
+					if ((grid[local_node.x+i][local_node.y+y].C<local_node.C)
+					&&(grid[local_node.x+i][local_node.y+y].state=='c')){
+
+						temp_node = grid[local_node.x+i][local_node.y+y];
+					}
+				//}
+			}
+		}
+		local_node = temp_node;
+		path[c_path]=local_node.x;
+		cout << "x: " << local_node.x << " ";
+		c_path++;
+		path[c_path]=local_node.y;
+		cout << "y: " <<local_node.y << endl;
+		c_path++;
+	}
+	if(file)
+		{
+	    	file.close();
+	    }
 }
